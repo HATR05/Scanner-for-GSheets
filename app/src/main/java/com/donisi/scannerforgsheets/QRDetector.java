b5f0dcfc1eed634e375df50f8155625f918e79e2
HugoTalledos <47650545+HugoTalledos@users.noreply.github.com>
2020-01-20 17:11:14 -0500

    Add files via upload

--- app/src/main/java/com/donisi/scannerforgsheets/QRDetector.java
+++ app/src/main/java/com/donisi/scannerforgsheets/QRDetector.java
@@ -1,28 +1,35 @@
 package com.donisi.scannerforgsheets;
 
 import android.content.Context;
+import android.os.Message;
 import android.util.Log;
 import android.util.SparseArray;
+import android.widget.Switch;
+import 	android.os.Handler;
+
 
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 
 
+
+
 public class QRDetector implements Detector.Processor {
 
-    private Context context;
     private String token;
-    private String tokenanterior;
+    private String token_anterior;
+    private String status_switch;
+    private Switch switch_mode;
+    private Context context;
+    private Handler handler;
 
-    public  QRDetector(Context context, String token){
+    QRDetector(Switch switch_mode, Context context) {
+        this.token = "";
+        this.token_anterior = "";
+        this.switch_mode = switch_mode;
+        this.status_switch = "";
         this.context = context;
-        this.token = token;
-        tokenanterior = "";
-    }
-
-    @Override
-    public void release() {
-
+        handler = new TestHandler(getContext());
     }
 
     @Override
@@ -30,22 +37,29 @@ public class QRDetector implements Detector.Processor {
         final SparseArray<Barcode> barcodes = detections.getDetectedItems();
 
         if (barcodes.size() > 0) {
+            setStatus_switch(switch_mode.isChecked()?",1":",0"); //Se revisa si esta en modo de entrada o salida
+            setToken( barcodes.valueAt(0).displayValue);    // obtenemos el token
+            if (!getToken().equals(getToken_anterior())) {
+                setToken_anterior(getToken());
+                Log.i("token", getToken()+getStatus_switch());
 
-            token = barcodes.valueAt(0).displayValue;   // obtenemos el token
-            if (!token.equals(tokenanterior)) {
-                tokenanterior = token;
-                Log.i("token", token);
-                Log.i("YAA", token);
+                //Preparación y envio de mensaje para notificación de usuario
+                Message msg = new Message();
+                msg.what = switch_mode.isChecked()?1:0;
+                handler.sendMessage(msg);
 
-
+                //Preparación para envio a google sheets
+                SendRequest send_request = new SendRequest(getToken()+getStatus_switch(), getContext());
+                send_request.execute(); //Hilo que realiza la petición y envio de información a google Sheets
 
                 new Thread(new Runnable() {
                     public void run() {
                         try {
                             synchronized (this) {
-                                wait(5000);
+                                wait(4000);
                                 // limpiamos el token
-                                tokenanterior = "";
+                                setToken_anterior("");
+                                setStatus_switch(switch_mode.isChecked()?",1":",0");
                             }
                         } catch (InterruptedException e) {
                             /* TODO Auto-generated catch block */
@@ -57,4 +71,27 @@ public class QRDetector implements Detector.Processor {
             }
         }
     }
+
+    //----------------------------------Getters and Settes -----------------------
+
+    private void setToken(String token) { this.token = token; }
+
+    private String getToken() { return this.token; }
+
+    private void setToken_anterior(String tokenanterior) { this.token_anterior = tokenanterior; }
+
+    private String getToken_anterior(){return this.token_anterior;}
+
+    private String getStatus_switch(){ return this.status_switch; }
+
+    private void setStatus_switch(String switch_mode){ this.status_switch = switch_mode;  }
+
+    private Context getContext(){ return this.context; }
+
+
+
+    //---------------------------------Métodos de interfaz ----------------------------
+
+    @Override
+    public void release() { }
 }
